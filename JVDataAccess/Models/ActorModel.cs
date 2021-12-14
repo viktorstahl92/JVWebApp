245d44c18f035822ccaf22427081e8f10de3969a
viktorstahl92 <info@viktorstahl.com>
2021-12-14 13:58:14 +0100

    Uppdaterat skådespelarinfo
    Lade in totala antalet spelade föreställningar
    och produktioner i Skådespelarinfon.

--- JVDataAccess/Models/ActorModel.cs
+++ JVDataAccess/Models/ActorModel.cs
@@ -9,6 +9,12 @@ namespace JVDataAccess.Models
     {
         public int ActorID { get; set; }
         public string Namn { get; set; }
+        public int TotalShowsPlayed { get; set; }
+        public int TotalProductions { get; set; }
+
+        public string GetTotalProductions => $"{TotalProductions} {(TotalProductions == 1 ? "produktion" : "produktioner")}";
+
+        public string GetTotalShowsPlayed => $"{TotalShowsPlayed} {(TotalShowsPlayed == 1 ? "föreställning" : "föreställningar")}";
 
     }
 }
