af06e9a0cb8bb4a857fa89015bf44767c293d894
viktorstahl92 <info@viktorstahl.com>
2021-12-16 17:24:53 +0100

    Uppdaterad skådespelarinfo
    Uppdateraed skådespelarinfon så att den
    också skriver ut hur många % av alla föreställningar
    man spelat tillsammans med den andre skådespelaren.

---
+++ JVWebApp/Calculations.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace JVWebApp
+{
+    public static class Calculations
+    {
+
+        public static string GetPercentage (int Lower, int Higher)
+        {
+            int totalPercentage = (int)((double)Lower / Higher * 100);
+
+            return $"{totalPercentage}%";
+        }
+
+
+    }
+}
--- JVWebApp/Pages/DataPages/ActorPageBase.cs
+++ JVWebApp/Pages/DataPages/ActorPageBase.cs
@@ -52,6 +52,7 @@ namespace JVWebApp.Pages.DataPages
             }
             catch
             {
+                
             }
         }
 
